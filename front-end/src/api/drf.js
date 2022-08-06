7dba180280502024b39eb150857e16067ab26358
h5282001 <h5282001@naver.com>
2022-08-06 22:45:32 +0900

    Test : 배포테스트

--- front-end/src/api/drf.js
+++ front-end/src/api/drf.js
@@ -1,23 +1,23 @@
-const BASE_URL = 'http://localhost:8000/'
-const ACCOUNTS = BASE_URL + 'accounts/'
-const NOTICE = BASE_URL + 'notice/'
+const BASE_URL = "http://3.36.69.192:8000/";
+const ACCOUNTS = BASE_URL + "accounts/";
+const NOTICE = BASE_URL + "notice/";
 
 export default {
   accounts: {
-    login: () => ACCOUNTS + 'login/',
-    logout: () => ACCOUNTS + 'logout/',
-    signup: () => ACCOUNTS + 'registration/',
-    schoolInfo: () => ACCOUNTS + 'schoolinfo/',
-    checkUsername: () => ACCOUNTS + 'check_username/',
-    findUsername: () => ACCOUNTS + 'find_username/',
-    sendEmail: () => ACCOUNTS + 'send_signup_email/',
-    sendPwEmail: () => ACCOUNTS + 'send_pw_email/',
-    changePw: () => ACCOUNTS + 'change_pw/',
+    login: () => ACCOUNTS + "login/",
+    logout: () => ACCOUNTS + "logout/",
+    signup: () => ACCOUNTS + "registration/",
+    schoolInfo: () => ACCOUNTS + "schoolinfo/",
+    checkUsername: () => ACCOUNTS + "check_username/",
+    findUsername: () => ACCOUNTS + "find_username/",
+    sendEmail: () => ACCOUNTS + "send_signup_email/",
+    sendPwEmail: () => ACCOUNTS + "send_pw_email/",
+    changePw: () => ACCOUNTS + "change_pw/",
   },
   notice: {
-    noticeList: () => NOTICE + 'main/',
-    noticeDetail: () => NOTICE + 'detail/',
-    noticeCreate: () => NOTICE + 'create/',
-    noticeUpdate: () => NOTICE + 'update/'
-  }
-}
+    noticeList: () => NOTICE + "main/",
+    noticeDetail: () => NOTICE + "detail/",
+    noticeCreate: () => NOTICE + "create/",
+    noticeUpdate: () => NOTICE + "update/",
+  },
+};
