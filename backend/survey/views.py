d5bc5eb55f29a0fd06f4f8d7f272d69bc5f55287
h5282001 <h5282001@naver.com>
2022-08-09 17:29:00 +0900

    Test : 오류 테스트1

--- backend/survey/views.py
+++ backend/survey/views.py
@@ -11,7 +11,7 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-import json
+
 class SurveyTeacherMainView(APIView) :
     ## 권한 설정 부분(View단위)
     # permission_classes = (IsAuthenticated,)
@@ -52,7 +52,6 @@ class SurveyCreateView(APIView):
 
         
         # survey_serializer
-
         # 설문조사 등록하기 start
         survey_serializer = SurveySerializer(data=req.data['survey'])
         if req.data['survey']['grade'] == 0:
@@ -170,23 +169,22 @@ class SurveyStatDetailView(APIView):
 class SurveySubmitView(APIView):
     def post(self, req):
 
-        answers = req.POST.getlist('answers')
+        answers = req.data['answers']
         survey = SurveyList.objects.get(id=req.data['survey_num'])
-        print("answers:")
-        print(answers)
-
+        
         userauth = survey.target.filter(username=req.user.username).exists()
         if not userauth:
             return Response({"message" : "설문 제출 자격이 없습니다."})
 
         done =  survey.done_target.filter(username=req.user.username).exists()
+        print(done)
         if done:
             return Response({"message" : "이미 제출하셨습니다."})
-
         for answer in answers:
-            print(answer['id'])
-            print(type(answer['id']))
+            print(answer)
             question = SurveyQuestions.objects.get(id=answer['id'])
+            
+
             
             if question.multiple_bogi is not None:
                 if answer['answer'] == 1:
