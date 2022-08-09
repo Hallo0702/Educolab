36f0bf073cce83c3d051e9017b35a700f0223ccc
wonkyoung <pubhan35@gmail.com>
2022-08-09 16:53:37 +0900

    Feat : 학생 자율학습 등록, 수정 구현 완료

--- backend/homework/views.py
+++ backend/homework/views.py
@@ -86,7 +86,6 @@ class HomeworkCreateView(APIView):
             if homework_serializer.is_valid(raise_exception=True):
                 teacher = UserInfo.objects.get(school=request.user.school,class_field=request.user.class_field,grade=request.user.grade,userflag=True)
                 homework = homework_serializer.save(student=request.user,teacher=teacher)
-
                 files = request.FILES.getlist("files")
                 for file in files:
                     fp = Files.objects.create(student_homework=homework,atch_file=file,atch_file_name=file)
@@ -100,22 +99,16 @@ class HomeworkCreateView(APIView):
             print(request.data)
             homework_serializer = TeacherHomeworkCreateSerializer(data=request.data)
             if homework_serializer.is_valid(raise_exception=True):
-                print('valid')
                 school = request.user.school
-                print(school)
                 school_student= school.school_student.all()
                 students = school_student.filter(grade=homework_serializer.validated_data['grade'],class_field=homework_serializer.validated_data['class_field'],userflag=False)
                 homework = homework_serializer.save(teacher=request.user,target=students)
-                print(homework)
                 files = request.FILES.getlist("files")
-                print(files)
                 for file in files:
-                    print(file)
                     fp = Files.objects.create(teacher_homework=homework,atch_file=file,atch_file_name=file)
                     fp.save()
                 
                 for student in students:
-                    print(student)
                     submit = SubmitHomework.objects.create(student=student,teacher_homework=homework)
                     submit.save()
 
