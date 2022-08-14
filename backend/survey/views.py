e0daa390f96164a8f30b6fd11725eca8c832680d
홍찬기 <sal0203@naver.com>
2022-08-14 19:31:02 +0900

    fix : 머지 충돌

--- backend/survey/views.py
+++ backend/survey/views.py
@@ -11,12 +11,7 @@ from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-<<<<<<<<< Temporary merge branch 1
 import os, io
-
-=========
->>>>>>>>> Temporary merge branch 2
-
 class SurveyTeacherMainView(APIView) :
     ## 권한 설정 부분(View단위)
     # permission_classes = (IsAuthenticated,)
