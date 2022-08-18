6cda58ee1beb7355c9b05b16046c0bbf6f6b3e67
홍찬기 <sal0203@naver.com>
2022-08-19 03:38:48 +0900

    fix : 파일주소 변경 실험

--- front-end/src/components/TaskTargetStudent.vue
+++ front-end/src/components/TaskTargetStudent.vue
@@ -10,7 +10,7 @@
             {{item.content}}
           <div>
             <div v-for="(file, idx) in item['atch_file_name']" :key="idx">
-              <a :href="url+item['atch_file'][idx]">{{file}}</a>
+              <a :href="item['atch_file'][idx]">{{file}}</a>
             </div>
           </div>
           <div v-if="!isChecked && !check.computedFlag" class="row justify-evenly">
